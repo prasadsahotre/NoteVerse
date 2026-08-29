6fd72d7bc63160d0e61574e65f468f47840ff41c
Prasad Sahotre <pvsahotre@gmail.com>
2026-08-29 22:29:14 +0530

    feat: add lesson database model

---
+++ backend/prisma/migrations/20260829165307_add_lesson/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Lesson" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "position" INTEGER NOT NULL,
+    "moduleId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Lesson_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Lesson" ADD CONSTRAINT "Lesson_moduleId_fkey" FOREIGN KEY ("moduleId") REFERENCES "Module"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
